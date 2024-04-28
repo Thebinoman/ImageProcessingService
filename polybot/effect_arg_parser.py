6769a4d28988e968012ba63de46d87399eca9c2c
Ofir Bino <thebinoman@gmail.com>
2024-04-28 19:32:37 +0300

    Beautify and add comments to effect_arg_parser.py

--- polybot/effect_arg_parser.py
+++ polybot/effect_arg_parser.py
@@ -1,17 +1,47 @@
+"""
+Effect Arg Parser
+A parser for effect commands
+"""
+
+# pylint: disable=E0401
 from polybot.effect_arg_rules import ArgRangeRule
 from polybot.error import CommandError
 from polybot.response_types import ErrorTypes
+# pylint: enable=E0401
 
 
+# pylint: disable=R0903
 class EffectArgParser:
-    def __init__(self, arg_amount_start: int, arg_amount_end: int, arg_rule_list: iter, multi: bool = False):
+    """
+    Parser for arguments of a command.
+    """
+
+    def __init__(self,
+                 arg_amount_start: int,
+                 arg_amount_end: int,
+                 arg_rule_list: iter,
+                 multi: bool = False):
         self.arg_amount_range = ArgRangeRule(arg_amount_start, arg_amount_end)
         self.arg_rule_list = arg_rule_list
         self.multi = multi
 
     def parse(self, arg_list: iter, effect_string: str):
-        if type(self.arg_amount_range.parse(len(arg_list), effect_string)) is CommandError:
-            return [CommandError(ErrorTypes.ARG_AMOUNT_ERROR, [effect_string, len(arg_list), self.arg_amount_range.start, self.arg_amount_range.end])]
+        """
+        Parse the args of a command by a list of given args.
+        :param arg_list: Required. List of args as strings.
+        :param effect_string: Required. The full command as a string.
+        :return: List of parsed values of the arguments, or CommandError instance of error found.
+        """
+
+        if isinstance(self.arg_amount_range.parse(len(arg_list), effect_string), CommandError):
+            return [
+                CommandError(
+                    ErrorTypes.ARG_AMOUNT_ERROR,
+                    [
+                        effect_string, len(arg_list),
+                        self.arg_amount_range.start,
+                        self.arg_amount_range.end
+            ])]
 
         parsed_args = []
         for i in range(min(len(arg_list), len(self.arg_rule_list))):
@@ -19,3 +49,4 @@ class EffectArgParser:
             parsed_args.append(self.arg_rule_list[i].parse(arg_list[i], effect_string))
 
         return parsed_args
+# pylint: enable=R0903
